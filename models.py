efd3bf5f6aa92b567b3e072eb2f2a4303278e7a3
sajan <sajanshrestharm11@gmail.com>
2020-04-03 16:56:55 -0400

    first commit

---
+++ models.py
@@ -0,0 +1,10 @@
+class Breed:
+
+    def __init__(self, name, size, height, weight, coat, energy, activities):
+        self.name = name
+        self.size = size
+        self.height = height
+        self.weight = weight
+        self.coat = coat
+        self.energy = energy
+        self.activities = activities
---
+++ scrape.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'https://www.purina.com'
+
+''' gets all the links from the current page '''
+
+
+def get_all_links(url):
+    html = requests.get(url).text
+    bs_obj = BeautifulSoup(html, 'lxml')
+
+    links = (link.attrs['href'] for link in bs_obj.findAll(
+        'a', attrs={'class': 'link'}) if 'href' in link.attrs)
+    return links
+
+
+''' goes to the link and gets the info '''
+
+
+def get_info(url):
+    html = requests.get(url).text
+    bs_obj = BeautifulSoup(html, 'lxml')
+
+    info = {}
+
+    info['name'] = bs_obj.h1.text
+
+    for row in bs_obj.findAll('dd', attrs={'class': 'statsDef-content-list-item'}):
+        key = row.find(
+            'div', {'class': 'statsDef-content-list-item-label'}).text
+        value = row.find(
+            'div', {'class': 'statsDef-content-list-item-value'}).text.strip()
+        info[key] = value
+
+    return info
+
+
+if __name__ == "__main__":
+    list_of_links = (get_all_links(f'{base_url}/dogs/dog-breeds?page={i}')
+                     for i in range(16))
+
+    for links in list_of_links:
+        for link in links:
+            print(get_info(base_url + link))
